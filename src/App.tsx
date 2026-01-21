0b94fc3232df778d29f623f5beb747e16e2bfc2f
airchry <iampty666@gmail.com>
2026-01-21 22:44:26 +0700

    change navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import HomeDefaultRoute from "./components/HomeDefaultRoute";
 import WelcomePage from "./pages/WelcomePage";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
 
       <Route element={<ProtectedRoute/>}>
+      <Header />
         <Route path="/welcome" element={<WelcomePage/>} />
         <Route path="/inputspd" element={<InputSPD />} />
         <Route path="/listspd" element={<ListPage />} />
--- src/pages/InputSPD.tsx
+++ src/pages/InputSPD.tsx
@@ -1,10 +1,8 @@
 import Input from '../components/Input'
-import Header from '../components/Header'
 
 function InputSPD() {
     return (
     <div className="flex flex-col bg-white">
-      <Header />
       <div className="w-full mx-auto py-10">
         <Input />
       </div>
