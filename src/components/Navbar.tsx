9a28a6065a2494c95ce03b1361312fe0eedeccfb
airchry <iampty666@gmail.com>
2026-01-21 19:52:13 +0700

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import api from "../api";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function Navbar() {
@@ -28,8 +28,8 @@ function Navbar() {
 
     return (
         <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-center lg:px-8 mx-6">
-            <a href="/inputspd" className="p-2 text-sm/6 font-semibold text-white hover:bg-yellow-500 hover:text-black">Input SPD</a>
-            <a href="/listspd" className="p-2 text-sm/6 font-semibold text-white hover:bg-yellow-500 hover:text-black">List SPD</a>
+            <Link to="/inputspd" className="p-2 text-sm/6 font-semibold text-white hover:bg-yellow-500 hover:text-black">Input SPD</Link>
+            <Link to="/listspd" className="p-2 text-sm/6 font-semibold text-white hover:bg-yellow-500 hover:text-black">List SPD</Link>
             {user ?
             <button onClick={handleLogout} className="p-2 text-sm/6 font-semibold text-white hover:bg-yellow-500 hover:text-black">Logout</button>
             : <button onClick={() => navigate("/login")} className="p-2 text-sm/6 font-semibold text-white hover:bg-yellow-500 hover:text-black">Login</button>
