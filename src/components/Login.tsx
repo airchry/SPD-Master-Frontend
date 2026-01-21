5338bf8a480f10dc4655817be9a17443bb9801f2
airchry <iampty666@gmail.com>
2026-01-21 22:33:49 +0700

    change the login button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -33,7 +33,7 @@ function Login() {
                         <input value={username} onChange={e => setUsername(e.target.value)} type="text" className="block w-full h-[30px] rounded-xs bg-black/1 px-3 text-base text-gray-600 outline-1 outline-offset-1 outline-black/20 placeholder:text-gray-500 focus:outline-1 focus:outline-indigo-500 sm:text-sm/6 mb-5"/>
                         <label className="mb-2 text-sm text-gray-600">Kata Sandi</label>
                         <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="block w-full h-[30px] rounded-xs bg-black/1 px-3 text-base text-gray-600 outline-1 outline-offset-1 outline-black/20 placeholder:text-gray-500 focus:outline-1 focus:outline-indigo-500 sm:text-sm/6 mb-5"/>
-                        <button type="submit" className="font-bold text-[#212c5f] mt-5 bg-[#ffc91b] w-full h-[30px] rounded-xs hover:bg-[#212c5f] hover:text-[#ffc91b]">Loginn</button>
+                        <button type="submit" className="font-bold text-[#212c5f] mt-5 bg-[#ffc91b] w-full h-[30px] rounded-xs">Login</button>
                     </form>
                     
                 </div>
